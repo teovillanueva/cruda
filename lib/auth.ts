1a8999c908fecc706fcdef3034af3f40d0bbe342
teovillanueva <teodoro2102@gmail.com>
2026-03-22 11:59:15 -0300

    fix: better auth url

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,7 +3,16 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { username } from "better-auth/plugins";
 import { db } from "./db";
 
+function getBaseURL() {
+  if (process.env.BETTER_AUTH_URL) return process.env.BETTER_AUTH_URL;
+  if (process.env.VERCEL_PROJECT_PRODUCTION_URL)
+    return `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`;
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`;
+  return "http://localhost:3000";
+}
+
 export const auth = betterAuth({
+  baseURL: getBaseURL(),
   database: drizzleAdapter(db, { provider: "pg" }),
   emailAndPassword: {
     enabled: true,
